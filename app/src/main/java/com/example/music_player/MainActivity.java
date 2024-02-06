23413ea1165db91883eeffa589a1ec7f16496c70
hemanathan18 <hemanathan1802@gmail.com>
2024-02-06 19:36:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/music_player/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.music_player;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import android.Manifest;
+
+
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.browse.MediaBrowser;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    TextView noMusicText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recyclerView);
+        noMusicText=findViewById(R.id.noSongs);
+
+        if(checkPermission()==false){
+            requestPermission();
+            return;
+        }
+
+        String[] projection={
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION,
+        };
+
+        //To access songs from local storage
+        Cursor cursor=getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,)
+    }
+
+    boolean checkPermission(){
+
+        int result= ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE );
+        if(result == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE))
+        {
+            Toast.makeText(MainActivity.this,"FILE READ ACCESS REQUIRED. ENABLE IT FROM SETTINGS",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+        }
+    }
+}
